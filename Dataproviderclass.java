b7bab1edf0dc62e02ad20d09ba9c06391beca9ac
Sankhadeep Pujaru <sankhadeep.pujaru2008@gmail.com>
2021-10-29 18:37:34 +0530

    Create Dataproviderclass.java

---
+++ Dataproviderclass.java
@@ -0,0 +1,15 @@
+package parameters;
+
+import org.testng.annotations.DataProvider;
+
+public class DataproviderClass {
+        @DataProvider(name="SearchProvider")
+        public static Object[][] getDataFromDataprovider(){
+            return new Object[][] 
+            {
+                { "Guru99", "India" },
+                { "Krishna", "UK" },
+                { "Bhupesh", "USA" }
+            };  
+        }
+}
